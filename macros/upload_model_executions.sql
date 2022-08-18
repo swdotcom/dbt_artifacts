1088a05cbc3dfe7a9442f3185546c1c8618737df
Michael Fagan <michael.g.fagan@gmail.com>
2022-08-18 16:30:33 -0500

    Fix comma

--- macros/upload_model_executions.sql
+++ macros/upload_model_executions.sql
@@ -116,7 +116,7 @@
             {{ adapter.dispatch('column_identifier', 'dbt_artifacts')(10) }},
             {{ adapter.dispatch('column_identifier', 'dbt_artifacts')(11) }},
             {{ adapter.dispatch('parse_json', 'dbt_artifacts')(adapter.dispatch('column_identifier', 'dbt_artifacts')(12)) }},
-            {{ adapter.dispatch('column_identifier', 'dbt_artifacts')(13) }},
+            {{ adapter.dispatch('column_identifier', 'dbt_artifacts')(13) }}
             {# {{ adapter.dispatch('parse_json', 'dbt_artifacts')(adapter.dispatch('column_identifier', 'dbt_artifacts')(14)) }} #}
         from values
         {% for model in results if model.node.resource_type == "model" -%}
