e3993ffced8352a295450444adc579760b1edb37
Michael Fagan <michael.g.fagan@gmail.com>
2022-08-18 15:50:05 -0500

    Update exposure macros

--- macros/create_exposures_table_if_not_exists.sql
+++ macros/create_exposures_table_if_not_exists.sql
@@ -16,7 +16,7 @@
 
 {%- endmacro %}
 
-{% macro snowflake__get_create_exposures_table_if_not_exists_statement(database_name, schema_name, table_name) -%}
+{% macro snowflake__get_create_exposures_table_if_not_exists_statement_test(database_name, schema_name, table_name) -%}
     create table {{database_name}}.{{schema_name}}.{{table_name}} (
         command_invocation_id STRING,
         node_id STRING,
@@ -33,7 +33,7 @@
     )
 {%- endmacro %}
 
-{% macro default__get_create_exposures_table_if_not_exists_statement(database_name, schema_name, table_name) -%}
+{% macro default__get_create_exposures_table_if_not_exists_statement_test(database_name, schema_name, table_name) -%}
     create table {{database_name}}.{{schema_name}}.{{table_name}} (
         command_invocation_id STRING,
         node_id STRING,
@@ -47,5 +47,22 @@
         url STRING,
         package_name STRING,
         depends_on_nodes STRING
+    )
+{%- endmacro %}
+
+
+{% macro snowflake__get_create_exposures_table_if_not_exists_statement(database_name, schema_name, table_name) -%}
+    create table {{database_name}}.{{schema_name}}.{{table_name}} (
+        command_invocation_id STRING,
+        run_started_at TIMESTAMP_NTZ,
+        exposure VARIANT
+    )
+{%- endmacro %}
+
+{% macro default__get_create_exposures_table_if_not_exists_statement(database_name, schema_name, table_name) -%}
+    create table {{database_name}}.{{schema_name}}.{{table_name}} (
+        command_invocation_id STRING,
+        run_started_at TIMESTAMP,
+        exposure STRING
     )
 {%- endmacro %}
