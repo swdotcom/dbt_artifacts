e24df211c9f8b762330f121fc3032812240c6335
Michael Fagan <michael.g.fagan@gmail.com>
2023-07-17 14:45:54 -0500

    Update thread_id data type

--- models/staging/stg_dbt__model_executions.sql
+++ models/staging/stg_dbt__model_executions.sql
@@ -33,7 +33,7 @@ enhanced as (
       , command_invocation_id
       , unique_id as node_id
       , adapter_response:query_id::string as query_id
-      , split(thread_id, '-')[1]::int as thread_id
+      , thread_id
       , run_started_at
       , compile_started_at
       , compile_completed_at
